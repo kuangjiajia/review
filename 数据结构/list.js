b0d1ed28d79b691a7e3d0d67bfced7ac51e9299e
kuangjiajia <1298201366@qq.com>
2019-03-17 22:56:42 +0800

    做了面试题

--- 数据结构/list.js
+++ 数据结构/list.js
@@ -49,44 +49,46 @@
 
 
 
-var findKthLargest = function (nums, k) {
-  adjust(nums) //建立大顶堆
-  let len = nums.length
-  let x = 1
-  for (let i = len - 1; i >= 0; i--) {
-    [nums[0], nums[i]] = [nums[i], nums[0]];
-    shift(nums, 0, i)
-    if (x === k) {
-      return nums[i]
-    }
-    x++
-  }
-}
+// var findKthLargest = function (nums, k) {
+//   adjust(nums) //建立大顶堆
+//   let len = nums.length
+//   let x = 1
+//   for (let i = len - 1; i >= 0; i--) {
+//     [nums[0], nums[i]] = [nums[i], nums[0]];
+//     shift(nums, 0, i)
+//     if (x === k) {
+//       return nums[i]
+//     }
+//     x++
+//   }
+// }
 
-function adjust(nums) {
-  let len = nums.length
-  let n = Math.floor((len - 1) / 2)
-  for (let i = n; i >= 0; i--) {
-    shift(nums, i, len)
-  }
-}
+// function adjust(nums) {
+//   let len = nums.length
+//   let n = Math.floor((len - 1) / 2)
+//   for (let i = n; i >= 0; i--) {
+//     shift(nums, i, len)
+//   }
+// }
 
-function shift(arr, i, len) {
-  let left = 2 * i + 1
-  let right = 2 * i + 2
-  let index = i
-  if (arr[left] > arr[index] && left < len) {
-    index = left
-  }
-  if (arr[right] > arr[index] && right < len) {
-    index = right
-  }
-  if (index !== i) {
-    [arr[i], arr[index]] = [arr[index], arr[i]];
-    shift(arr, index, len)
-  }
-}
+// function shift(arr, i, len) {
+//   let left = 2 * i + 1
+//   let right = 2 * i + 2
+//   let index = i
+//   if (arr[left] > arr[index] && left < len) {
+//     index = left
+//   }
+//   if (arr[right] > arr[index] && right < len) {
+//     index = right
+//   }
+//   if (index !== i) {
+//     [arr[i], arr[index]] = [arr[index], arr[i]];
+//     shift(arr, index, len)
+//   }
+// }
 
-console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
+// console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
 
-console.log(123)
+// console.log(123)
+
+
